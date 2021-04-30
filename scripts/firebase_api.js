104893266d9edb394f147fa804ea838e484ae87f
Jackson Marsnas <jmarsnas@my.bcit.ca>
2021-04-30 10:40:04 -0700

    Added firstore stuff

---
+++ scripts/firebase_api.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCgER9S4FBPGbUg6yHZYtsPxALhZ8BzDDk",
+    authDomain: "fiendship-85fa8.firebaseapp.com",
+    projectId: "fiendship-85fa8",
+    storageBucket: "fiendship-85fa8.appspot.com",
+    messagingSenderId: "462108145345",
+    appId: "1:462108145345:web:ddf1ded09345965d188640"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
