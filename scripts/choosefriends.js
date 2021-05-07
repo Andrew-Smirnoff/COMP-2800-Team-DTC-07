a897c5d0d590cc65b9869d087e81dce23fceac0b
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-05-07 14:05:18 -0700

    Display friends array

--- scripts/choosefriends.js
+++ scripts/choosefriends.js
@@ -1,7 +1,11 @@
 let friends_list = sessionStorage.getItem('friends')
+console.log(friends_list)
+console.log(typeof(friends_list))
+var res = friends_list.split(",")
+console.log(res)
 let form = document.querySelector('form')
-console.log(friends_list.length)
+console.log(res.length)
 
-for(i = 0; i < friends_list.length; i++){
-    console.log(friends_list[i])
+for(i = 0; i < res.length; i++){
+    console.log(res[i])
 }
