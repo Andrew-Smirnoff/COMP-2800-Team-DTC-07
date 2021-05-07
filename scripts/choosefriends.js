db0445e1047998e513d85c72c8f8198832a06aa1
Jackson Marsnas <jmarsnas@my.bcit.ca>
2021-05-07 15:08:50 -0700

    Add scrolling form

--- scripts/choosefriends.js
+++ scripts/choosefriends.js
@@ -1,14 +1,21 @@
-let friends_list = sessionStorage.getItem('friends')
-friends_list = friends_list.split(",")
-let form = document.querySelector('form')
+main();
 
-for(i = 0; i < friends_list.length; i++){
-    let new_box = document.createElement('input');
-    let new_label = document.createElement('label');
-    new_box.setAttribute('type', 'checkbox')
-    new_box.setAttribute('name', friends_list[i])
-    new_label.setAttribute('for', friends_list[i])
-    new_label.innerHTML = friends_list[i]
-    form.appendChild(new_box)
-    form.appendChild(new_label)
+function main(){
+    before_game();
+}
+
+function before_game(){
+    let friends_list = sessionStorage.getItem('friends')
+    friends_list = friends_list.split(",")
+    let form = document.querySelector('form')
+    for(i = 0; i < friends_list.length; i++){
+        let new_box = document.createElement('input');
+        let new_label = document.createElement('label');
+        new_box.setAttribute('type', 'checkbox')
+        new_box.setAttribute('name', friends_list[i])
+        new_label.setAttribute('for', friends_list[i])
+        new_label.innerHTML = friends_list[i]
+        form.appendChild(new_label)
+        form.appendChild(new_box)
+    }
 }
