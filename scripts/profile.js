a22c747b46039132ba0003d3f69af1b4cd94a0df
Jackson Marsnas <jmarsnas@my.bcit.ca>
2021-05-07 15:55:39 -0700

    Add to profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,2 +1,6 @@
-$("#name_goes_here").text(sessionStorage.getItem('name'));
-$("#coins_go_here").text(sessionStorage.getItem('name'));
+main();
+
+function main(){
+    $("#name_goes_here").text(sessionStorage.getItem('name'));
+    $("#coins_go_here").text(sessionStorage.getItem('name'));
+}
