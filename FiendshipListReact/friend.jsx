9e2dbd878b6dcd8a0283b08d43336b86d133b229
denes <denesgyorgy97@gmail.com>
2021-05-11 09:35:38 -0700

    The start of the Fiendship app

---
+++ FiendshipListReact/friend.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Friend extends Component {
+    state = { 
+        name: this.props.value
+     }
+    render() { 
+        return ;
+    }
+}
+ 
+export default Friend;
