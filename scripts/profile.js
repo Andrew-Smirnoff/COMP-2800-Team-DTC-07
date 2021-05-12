6fc224497be7794b94d5031aebd1ca5f5d82f849
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-05-12 10:17:07 -0700

    Add coin display in profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -2,5 +2,5 @@ main();
 
 function main(){
     $("#name_goes_here").text(sessionStorage.getItem('name'));
-    $("#coins_go_here").text(sessionStorage.getItem('name'));
+    $("#coins_go_here").text(sessionStorage.getItem('coins'));
 }
