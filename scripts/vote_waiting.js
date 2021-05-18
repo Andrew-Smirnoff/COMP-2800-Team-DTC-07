e89c9d0b711b4d1a99a0a61891e0dcd0f13b07a6
Jackson Marsnas <jmarsnas@my.bcit.ca>
2021-05-18 14:21:22 -0700

    Fix vote_waiting screen

--- scripts/vote_waiting.js
+++ scripts/vote_waiting.js
@@ -1,17 +1,21 @@
 const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-  }
-  
-  const refresh = async () => {
-    while (1 > 0) {
-      await sleep(2000)
-      db.collection("rooms").doc(sessionStorage.getItem('room')).get().then(function (snap) {
-        let votes = snap.data()['votes']
-        if (votes == snap.data()['players'].length) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+const refresh = async () => {
+  while (1 > 0) {
+    await sleep(2000)
+    db.collection("rooms").doc(sessionStorage.getItem('room')).get().then(function (snap) {
+      let votes = snap.data()['votes']
+      if (votes == snap.data()['players'].length) {
+        if (Math.floor(snap.data()['rounds'] / snap.data()['players'].length) == 0) {
+          document.location.href = "./winner.html";
+        } else {
           document.location.href = "./results.html";
         }
-        next_page = true;
-      })
-    }
+      }
+      next_page = true;
+    })
   }
-  refresh()
+}
+refresh()
