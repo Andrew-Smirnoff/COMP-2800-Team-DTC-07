b73461ce0e5fbd729046bc72d954f1f9bd61ef80
KrisLiu2020 <77430778+KrisLiu2020@users.noreply.github.com>
2021-05-19 10:01:45 -0700

    Change user profile pic to dynamic display

--- scripts/profile.js
+++ scripts/profile.js
@@ -4,3 +4,29 @@ function main(){
     $("#name_goes_here").text(sessionStorage.getItem('name'));
     $("#coins_go_here").text(sessionStorage.getItem('coins'));
 }
+
+function getDocumentId() {
+    firebase.auth().onAuthStateChanged((user) => {
+      let document_id = user.uid
+      console.log('document_id: ', document_id)
+      sessionStorage.setItem('document_id', document_id)
+    });
+  }
+getDocumentId();
+
+function getCurrentProfilePic() {
+    let document_id = sessionStorage.getItem('document_id');
+    var docRef = db.collection('users').doc(document_id);
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            console.log('[user current profile pic] ', doc.data().current_profile_pic);
+            let user_current_profile_pic = doc.data().current_profile_pic;
+            $('#profile_pic').attr('src', user_current_profile_pic);
+        } else {
+            console.log('no such document')
+        }
+    }).catch((error)=> {
+        console.log('Error getting document: ', error)
+    })
+}
+getCurrentProfilePic();
