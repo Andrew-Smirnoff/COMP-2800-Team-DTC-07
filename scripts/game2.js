e4d13c3ad41c90bf74eafeb784f895667df0287b
Jackson Marsnas <jmarsnas@my.bcit.ca>
2021-05-20 15:10:48 -0700

    Organize code in main.js

--- scripts/game2.js
+++ scripts/game2.js
@@ -1,26 +1,51 @@
+function display_scenario(snap){
+    let scenario_index = Math.floor(snap.data()['rounds'] / snap.data()['stories'].length) - 1;
+    $("#scenario-goes-here").text(snap.data()['scenarios'][scenario_index])
+}
+
+function display_players(snap){
+    let players = snap.data()['players']
+    let stories = snap.data()['stories']
+    for (i = 0; i < players.length; i++) {
+        let new_image = document.createElement('img')
+        let new_li = document.createElement('li')
+        let ul = document.querySelector('ul')
+        new_image.setAttribute("src", stories[i]['picture'])
+        new_li.setAttribute('class', 'player')
+        new_li.setAttribute('id', i)
+        ul.appendChild(new_li)
+        $("#" + i).html(new_image)
+        $("#" + i).append(players[i])
+    }
+}
+
+function display_page_info(){
+    db.collection("rooms").doc(sessionStorage.getItem('room')).get().then(function (snap) {
+        display_scenario(snap)
+        display_players(snap)
+    })
+}
+
+function submit_story(){
+    let all_stories = []
+    let story = $("#story").val();
+    db.runTransaction((transaction) => {
+        return transaction.get(db.collection('rooms').doc(sessionStorage.getItem('room'))).then(function(snap){
+            all_stories = snap.data()['stories']
+            for(i = 0; i < all_stories.length; i++){
+                if(all_stories[i]['name'] == sessionStorage.getItem('name')){
+                    all_stories[i]['story'] = story;
+                }
+            }
+            transaction.update(db.collection('rooms').doc(sessionStorage.getItem('room')), {stories: all_stories})
+        })
+    }).then(function(){
+        document.location.href = "./answer_waiting.html";
+    })
+}
+
 function main(){
-    db.collection("rooms").doc(sessionStorage.getItem('room')).get().then(function (snap) {
-        let scenario_index = Math.floor(snap.data()['rounds'] / snap.data()['stories'].length) - 1;
-        $("#scenario-goes-here").text(snap.data()['scenarios'][scenario_index])
-    })
-
-    // setting list of names
-    db.collection("rooms").doc(sessionStorage.getItem('room')).get().then(function (snap) {
-        let players = snap.data()['players']
-        let stories = snap.data()['stories']
-        for (i = 0; i < players.length; i++) {
-            let new_image = document.createElement('img')
-            let new_li = document.createElement('li')
-            let ul = document.querySelector('ul')
-            new_image.setAttribute("src", stories[i]['picture'])
-            new_li.setAttribute('class', 'player')
-            new_li.setAttribute('id', i)
-            ul.appendChild(new_li)
-            $("#" + i).html(new_image)
-            $("#" + i).append(players[i])
-        }
-    })
-
+    display_page_info();
     $("#submit-btn").click(function(){
         let all_stories = []
         let story = $("#story").val();
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-startApp();
-delete_games();
-
 function make_date(){
     let today = new Date();
     let day = String(today.getDate())
@@ -12,12 +9,12 @@ function make_date(){
 
 function date_difference(full_date, document_date){
     full_date = full_date.split('/')
-            full_date = Number(full_date[0] + full_date[1] + full_date[2])
+    full_date = Number(full_date[0] + full_date[1] + full_date[2])
 
-            document_date = document_date.split('/')
-            document_date = Number(document_date[0] + document_date[1] + document_date[2])
+    document_date = document_date.split('/')
+    document_date = Number(document_date[0] + document_date[1] + document_date[2])
 
-            return document_date - full_date;
+    return document_date - full_date;
 }
 
 //delete old games
@@ -36,13 +33,10 @@ function delete_games(){
     })
 }
 
-function startApp() {
+function setup_player() {
     firebase.auth().onAuthStateChanged(function (user) {        // Check the user that's logged in
         if (user) {
-            db.collection('users')
-                .doc(user.uid)                                  // the user's UID
-                .get()                                          //READ !!
-                .then(function (doc) {
+            db.collection('users').doc(user.uid).get().then(function (doc) {
                     var picture = doc.data().current_profile_picture;
                     var name = doc.data().name;                 // point to user's name in the document
                     var friends = doc.data().friends;
@@ -57,13 +51,37 @@ function startApp() {
     })
 }
 
-// press start button
-$("#start").click(function(){
+function join_game(snap){
+    if(snap.data()['started'] == false){
+        sessionStorage.setItem('is_host', false)
+        let players = snap.data()['players']
+        let stories = snap.data()['stories']
+        let dict = {'name': sessionStorage.getItem('name'), 'story': "", "points": 0, "current_points": 0, "current_round": 0, "picture": sessionStorage.getItem("Current_Profile_Pic")}
+        stories.push(dict)
+        players.push(sessionStorage.getItem('name'))
+        transaction.update(db.collection('rooms').doc(room_number), {players: players, stories: stories})
+    } else if(snap.data()['started'] == true){
+        //show snackbar - it's snack time BABY
+        let snack = document.getElementById('snackbar')
+        snack.className = "show"
+        setTimeout(function(){snack.className = snack.className.replace("show", "");}, 3000)
+    }
+}
+
+function host_game(snap){
+    sessionStorage.setItem('is_host', true)
+    players_waiting = []
+    let full_date = make_date();
+    let dict = {'name': sessionStorage.getItem('name'), 'story': "", "points": 0, "current_points": 0, "current_round": 0, "picture": sessionStorage.getItem("Current_Profile_Pic")}
+    transaction.set(db.collection('rooms').doc(room_number), {players: [sessionStorage.getItem('name')],
+    room_number: room_number, stories: [dict], votes: 0, created: full_date, started: false})
+}
+
+function begin_game(){
     let room_number = $("#room_number").val();
     db.runTransaction((transaction) => {
         return transaction.get(db.collection('rooms').doc(room_number)).then(function(snap){
             if(snap.exists){
-                console.log(snap.data()['stories'].length)
                 if(snap.data()['started'] == false){
                     sessionStorage.setItem('is_host', false)
                     let players = snap.data()['players']
@@ -81,11 +99,7 @@ $("#start").click(function(){
             } else {
                 sessionStorage.setItem('is_host', true)
                 players_waiting = []
-                let today = new Date();
-                let day = String(today.getDate())
-                let month = String(today.getMonth())
-                let year = String(today.getFullYear())
-                let full_date = year + '/' + month + '/' + day
+                let full_date = make_date();
                 let dict = {'name': sessionStorage.getItem('name'), 'story': "", "points": 0, "current_points": 0, "current_round": 0, "picture": sessionStorage.getItem("Current_Profile_Pic")}
                 transaction.set(db.collection('rooms').doc(room_number), {players: [sessionStorage.getItem('name')],
                 room_number: room_number, stories: [dict], votes: 0, created: full_date, started: false})
@@ -93,11 +107,20 @@ $("#start").click(function(){
         })
     }).then(function(){
         db.collection('rooms').doc($("#room_number").val()).get().then(function(snap){
-            console.log(snap.data()['started'])
             if(snap.data()['started'] == false){
                 sessionStorage.setItem('room', room_number)
                 document.location.href = "./waiting.html";
             }
         })
     })
-})
+}
+
+function main(){
+    setup_player();
+    delete_games();
+    $("#start").click(function(){
+        begin_game();
+    })
+}
+
+main();
