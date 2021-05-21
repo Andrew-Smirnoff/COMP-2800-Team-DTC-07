d7e7e58a6078cde0b2bd88cdeb9cff66121e35a6
Jackson Marsnas <jmarsnas@my.bcit.ca>
2021-05-21 15:11:21 -0700

    Names display in order on winner.html

--- scripts/winner.js
+++ scripts/winner.js
@@ -15,8 +15,8 @@ function bubbleSort(players){
 db.collection('rooms').doc(sessionStorage.getItem('room')).get().then(function(snap){
     let all_players = snap.data()['stories']
     let sorted_players = bubbleSort(all_players)
-    console.log(sorted_players)
-    for(i = 0; i < snap.data()['stories'].length; i++){
+    for(i = sorted_players.length - 1; i >= 0 ; i--){
+        console.log(sorted_players)
         let new_item = document.createElement('li')
         new_item.innerHTML = sorted_players[i]['name'] + " ended with " + sorted_players[i]['points'] + " points."
         $("ul").append(new_item)
